7fbca70558b90c0113717d0c1d751134fb202cd0
clatran <clatran@ebay.com>
2017-11-13 16:19:39 +1100

    updating docs

--- README.rst
+++ README.rst
@@ -3,6 +3,16 @@ Funniest
 
 Example from http://python-packaging.readthedocs.io/en/latest/minimal.html
 
+To install:
+
+```
+   pip install "git+https://github.com/claritee/funniest.git"
+
+   or
+
+   sudo -H pip install "git+https://github.com/claritee/funniest.git"
+```
+
 To use (with caution), simply do::
 
     >>> import funniest
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='funniest',
       version='0.1',
       description='The funniest joke in the world',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/claritee/funniest',
       author='Flying Circus',
       author_email='flyingcircus@example.com',
       packages=['funniest'],
